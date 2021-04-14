53127257d5adf7d8cb54048dab319999e56ac07c
Chris Husky <chrishuskywolf@gmail.com>
2021-04-14 18:45:39 +0200

    fixed PlayerController + Walk Animation

--- AstrayDevProject/Assets/Scripts/Player/PlayerController.cs
+++ AstrayDevProject/Assets/Scripts/Player/PlayerController.cs
@@ -4,38 +4,33 @@ public class PlayerController : MonoBehaviour
 {
 
     public PlayerMovement playerMovement;
-    Vector2 movement;
-    Vector2 mousePos;
-    Vector2 dir;
-    float Zangle;
-    float MovementSpeed = 10;
 
     private void Update()
     {
 
         if(Input.GetKey(KeyCode.LeftShift)) {
 
-            MovementSpeed = 20;
+            playerMovement.MovementSpeed = 10;
 
         } else {
 
-            MovementSpeed = 10;
+            playerMovement.MovementSpeed = 5;
 
         }
 
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        playerMovement.movement.x = Input.GetAxisRaw("Horizontal");
+        playerMovement.movement.y = Input.GetAxisRaw("Vertical");
 
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        dir = mousePos - playerMovement.Player.position;
-        Zangle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        playerMovement.mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        playerMovement.dir = playerMovement.mousePos - playerMovement.Player.position;
+        playerMovement.Zangle = Mathf.Atan2(playerMovement.dir.y, playerMovement.dir.x) * Mathf.Rad2Deg;
 
     }
 
     private void FixedUpdate()
     {
 
-        playerMovement.Move(movement, Zangle, MovementSpeed);
+        playerMovement.Move();
 
     }
 
--- AstrayDevProject/Assets/Scripts/Player/PlayerMovement.cs
+++ AstrayDevProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    public Vector2 movement;
+    public Vector2 mousePos;
+    public Vector2 dir;
+    public float Zangle;
+    public float MovementSpeed = 10;
+
     //Components
     public Rigidbody2D Player;
     public Animator animator;
 
-    public void Move(Vector2 movement, float Zangle, float MovementSpeed)
+    public void Move()
     {
+
+        if(movement.x == 0 && movement.y == 0) {
+
+            animator.SetBool("Moving", false);
+
+        } else if(movement.x != 0 || movement.y != 0) {
+
+            animator.SetBool("Moving", true);
+
+        }
 
         Player.MovePosition(Player.position + movement * MovementSpeed * Time.fixedDeltaTime);
         Player.rotation = Zangle;
