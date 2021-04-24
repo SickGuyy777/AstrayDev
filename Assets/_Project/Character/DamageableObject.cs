8d391aed533bc4dfbdf65dce382c3b14d62c78e5
TrimericDragon7 <idansahar22@gmail.com>
2021-04-24 17:40:40 +0300

    Added HealthManager

--- Assets/_Project/Character/DamageableObject.cs
+++ Assets/_Project/Character/DamageableObject.cs
@@ -5,14 +5,14 @@ public class DamageableObject : MonoBehaviour, IDamageable
 {
     [SerializeField] private float damageMultiplier = 1;
     
-    public event Action<float> OnDamaged = delegate { };
+    public event Action<float, DamageableObject> OnDamaged = delegate { };
     public bool IsCritical => damageMultiplier > 1;
 
 
     public void Damage(float amount)
     {
         float totalDamage = GetDamage(amount);
-        OnDamaged?.Invoke(totalDamage);
+        OnDamaged?.Invoke(totalDamage, this);
     }
 
     public float GetDamage(float damage) => damage * damageMultiplier;
---
+++ Assets/_Project/Character/HealthManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    [Header("Setup")]
+
+    [SerializeField]
+    private List<DamageableObject> damageables = new List<DamageableObject>();
+
+    [SerializeField]
+    private bool getFromChildren;
+
+    [SerializeField]
+    private float health = 100f;
+
+    [SerializeField]
+    private float maxHealth = 120f;
+
+    public float Health
+    {
+        get => health;
+        set
+        {
+            if (value == health)
+                return;
+            value = Mathf.Clamp(value, 0, maxHealth);
+            HealthChanged?.Invoke();
+
+            if (value <= 0)
+            {
+                Die?.Invoke();
+                return;
+            }
+
+            health = value;
+        }
+
+    }
+
+    public event System.Action HealthChanged;
+    public event System.Action Die;
+
+    private void Start()
+    {
+        Health = Mathf.Clamp(Health, 0, maxHealth);
+
+        if (getFromChildren)
+        {
+            DamageableObject[] components = GetComponentsInChildren<DamageableObject>();
+            for (int i = 0; i < components.Length; i++)
+                damageables.Add(components[i]);
+        }
+
+        foreach (var damageable in damageables)
+            damageable.OnDamaged += Damaged;
+    }
+
+    private void Damaged(float damage, DamageableObject obj) => health -= damage;
+}
