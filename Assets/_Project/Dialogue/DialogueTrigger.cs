6072cd66aeed9ca7178675458886aa6db752f2b9
Justin Jones <54047625+SizzlingMelon@users.noreply.github.com>
2021-04-29 15:02:46 -0400

    Fixed Dialogue

--- Assets/_Project/Dialogue/DialogueTrigger.cs
+++ Assets/_Project/Dialogue/DialogueTrigger.cs
@@ -7,30 +7,20 @@ public class DialogueTrigger : MonoBehaviour, IInteractable
     public Dialogue dialogue;
     private ISelectionResponse selectionResponse;
 
-
-
-    public virtual void Awake(){
-        selectionResponse = GetComponent<ISelectionResponse>();
-    }
-
-
-    public void TriggerDialogue(){
-        FindObjectOfType<DialogueManager>().startDialogue(dialogue);
-    }
-
-    public void Select(){
+    
+    public void Select()
+    {
         selectionResponse?.OnSelect();
     }
-    public void Deselect(){
+    
+    public void Deselect()
+    {
         selectionResponse?.OnDeselect();
     }
-    public void Interact(PlayerController player){
+    
+    public void Interact(PlayerController player)
+    {
         Debug.Log("test direct");
-        TriggerDialogue();
     }
-    
-    
-
-    
 }
 
