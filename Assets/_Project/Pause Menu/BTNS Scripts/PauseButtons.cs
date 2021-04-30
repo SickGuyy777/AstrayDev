9a077aa2eb1d94de385c4af3a3fe28ccb15c0d1c
MohammadItani11 <itanim257@gmail.com>
2021-04-30 21:31:58 +0300

    Exit Pause Menu

--- Assets/_Project/Pause Menu/BTNS Scripts/PauseButtons.cs
+++ Assets/_Project/Pause Menu/BTNS Scripts/PauseButtons.cs
@@ -6,25 +6,48 @@ using UnityEngine.SceneManagement;
 public class PauseButtons : MonoBehaviour
 {
     public GameObject PausePanel;
-    public GameObject PauseBTN;
+    public GameObject ExitPanel;
+    public bool ispausemenu;
     public void Start()
     {
         PausePanel.SetActive(false);
+        ExitPanel.SetActive(false);
         Time.timeScale = 1;
     }
-    public void PauseButton()
+    public void Update()
     {
-        Time.timeScale = 0.01f;
-        PausePanel.SetActive(true);
-        PauseBTN.SetActive(false);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (ispausemenu == false)
+            {
+                ispausemenu = true;
+                Time.timeScale = 0.01f;
+                PausePanel.SetActive(true);
+            }
+            else
+            {
+                ispausemenu = false;
+                Time.timeScale = 1f;
+                PausePanel.SetActive(false);
+                ExitPanel.SetActive(false);
+            }
+        }
     }
     public void ResumeButton()
     {
         Time.timeScale = 1.0f;
         PausePanel.SetActive(false);
-        PauseBTN.SetActive(true);
+        ispausemenu = false;
     }
     public void HomeButton()
+    {
+        ExitPanel.SetActive(true);
+    }
+    public void No()
+    {
+        ExitPanel.SetActive(false);
+    }
+    public void Yes()
     {
         SceneManager.LoadScene(0);
     }
