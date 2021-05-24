9530316cdf84bd5c8be1eb2af7ac72b6a8118977
TrimericDragon7 <idansahar22@gmail.com>
2021-05-24 22:08:34 +0300

    Revert "Revert "Room System + API Update""
    This reverts commit 41d5467cc4b44fda1f8d99df6ce40a1494e42cb6.

--- Assets/_Project/Environment/Room System/Room.cs
+++ Assets/_Project/Environment/Room System/Room.cs
@@ -1,67 +1,99 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
+using System;
+using System.Linq;
 
 public class Room : MonoBehaviour
 {
     public UnityEvent<GameObject> OnObjectEnter;
     public UnityEvent<GameObject> OnObjectExit;
 
-    public string[] tagList;
+    public static event Action<Room, GameObject> OnRoomEntered;
+    public static event Action<Room, GameObject> OnRoomExited;
 
     [HideInInspector]
     public List<GameObject> objectsInsideRoom;
 
-    [HideInInspector]
-    public bool useBlackList;
-
     [SerializeField]
     private bool showGizmos;
 
-    [SerializeField]
-    private Vector2Int scale;    
-
-    private void Start()
-    {
-        TickClock.OnTick += UpdateLoop;
-    }
+    private void Start() => TickClock.OnTick += UpdateLoop;
 
     private void UpdateLoop()
     {
         var hits = Physics2D.OverlapBoxAll(transform.position, transform.localScale, transform.eulerAngles.z);
         if (hits.Length <= 0)
         {
-            for (int i = 0; i < objectsInsideRoom.Count; i++)
-            {
-
-            }
+            foreach (GameObject obj in objectsInsideRoom.ToList())
+                RemoveFromRoom(obj);
 
             return;
         }
+
+        if (hits.Length == 1)
+        {
+            var hit = hits[0].gameObject;
+
+            bool clearObjects = objectsInsideRoom.Count > 1 || objectsInsideRoom.Count > 0 && objectsInsideRoom[0] != hit;
+            if (clearObjects)
+            {
+                foreach (GameObject obj in objectsInsideRoom.ToList())
+                {
+                    bool isHit = hit == obj;
+                    if (!isHit)
+                        RemoveFromRoom(obj);
+                }
+            }
+
+            AddToRoom(hit.gameObject);
+
+            return;
+        }
+
+        // hits.Lenght > 1
+        var hitObjects = new List<GameObject>();
+        foreach (Collider2D hitCollider in hits)
+        {
+            var hit = hitCollider.gameObject;
+            AddToRoom(hit);
+
+            hitObjects.Add(hit);
+        }
+
+        foreach (GameObject obj in objectsInsideRoom.ToList())
+        {
+            bool isNoLongerInRoom = !hitObjects.Contains(obj);
+            if (isNoLongerInRoom)
+                RemoveFromRoom(obj);
+        }
+
+        void RemoveFromRoom(GameObject obj)
+        {
+            if (!IsInRoom(obj))
+                return;
+
+            OnObjectExit.Invoke(obj);
+            OnRoomExited?.Invoke(this, obj);
+            objectsInsideRoom.Remove(obj);
+        }
+
+        void AddToRoom(GameObject obj)
+        {
+            if (IsInRoom(obj))
+                return;
+
+            OnObjectEnter.Invoke(obj);
+            OnRoomEntered?.Invoke(this, obj);
+            objectsInsideRoom.Add(obj);
+        }
+
+        bool IsInRoom(GameObject obj) => objectsInsideRoom.Contains(obj);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnDrawGizmos()
     {
-        var contains = tagList.Contains(collision.tag);
-        if (tagList.Length == 0
-                || contains && useBlackList || !contains && !useBlackList)
-        {
-            OnObjectEnter.Invoke(collision.gameObject);
-
-            if (!objectsInsideRoom.Contains(collision.gameObject))
-                objectsInsideRoom.Add(collision.gameObject);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        var contains = tagList.Contains(collision.tag);
-        if (tagList.Length == 0
-                || contains && useBlackList || !contains && !useBlackList)
-        {
-            OnObjectExit.Invoke(collision.gameObject);
-            objectsInsideRoom.Remove(collision.gameObject);
-        }
+        if (showGizmos)
+            Gizmos.DrawWireCube(transform.position, transform.localScale);
     }
 }
